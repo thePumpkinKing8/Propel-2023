f3a9ead8e27b78dfc42363034c9f4177f8c8c0e9
Blake Laprade <lapradeblake@gmail.com>
2023-11-16 12:27:59 -0700

    direction buttons
    D-pad like button set that send out corresponding directions when pressed

---
+++ PropelJam2023/Assets/Scripts/UpDownLeftRight.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UpDownLeftRight : MonoBehaviour
+{
+    public UnityEvent<Vector3> DirectionPressed;
+    public UnityEvent<Vector3> SendDirection;
+    [SerializeField] private Direction direction;
+
+    private Vector3[] _direction = new Vector3[5]
+        {
+            Vector3.up,
+            Vector3.down,
+            Vector3.left,
+            Vector3.right,
+            Vector3.zero
+        };
+
+    private void OnMouseDown()//sends the chosen direction
+    {
+        DirectionPressed.Invoke(_direction[((int)direction)]);
+    }
+
+    public void ButtonPressed(Vector3 value) //calls parent to send direction
+    {
+        SendDirection.Invoke(value);
+    }
+
+}
+public enum Direction
+{ 
+    up,
+    down,
+    left,
+    right,
+    none
+}
+
