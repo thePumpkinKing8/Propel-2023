5286612ffc29d3b5483d0bb9977d80e78cb0faad
anna <annavgk@gmail.com>
2023-11-16 14:04:34 -0700

    Pause menu prefab and UI script
    Pause menu with functioning buttons

---
+++ PropelJam2023/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public bool SettingsOpen = false;
+    public bool GameOverOpen = false;
+    public bool PauseOpen = false;
+
+    [SerializeField]
+    public GameObject SettingsScreen;
+    public GameObject PauseScreen;
+    public GameObject GameOverScreen;
+    public AudioClip ButtonClick;
+    public AudioClip PlayClick;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+    public void Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        TogglePause();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+    public void ToggleSettings()
+    {
+        SettingsOpen = !SettingsOpen;
+        SettingsScreen.SetActive(SettingsOpen);
+    }
+
+    public void TogglePause()
+    {
+        PauseOpen = !PauseOpen;
+        PauseScreen.SetActive(PauseOpen);
+    }
+
+    public void OnStartButton()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+
+    public void OnExitButton()
+    {
+        Application.Quit();
+        EditorApplication.isPlaying = false;
+    }
+
+}
