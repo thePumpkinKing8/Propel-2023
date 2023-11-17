c5b7d725a55af6c173c00fb6a1209aef13fe4252
Blake Laprade <lapradeblake@gmail.com>
2023-11-16 21:12:11 -0700

    Changed Button to ButtonObject

--- PropelJam2023/Assets/Scripts/Switches/ButtonObject.cs
+++ PropelJam2023/Assets/Scripts/Switches/ButtonObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-public class Button : MonoBehaviour
+public class ButtonObject : MonoBehaviour
 {
     public UnityEvent OnPressed;
 
