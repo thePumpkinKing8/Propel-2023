c6c3b478e57b92d59e7f242f6885a9180c6fcd45
Blake Laprade <lapradeblake@gmail.com>
2023-11-16 23:01:32 -0700

    Elevator
    can be hooked up to a button, a lever or a a rotator(currently buggy).

---
+++ PropelJam2023/Assets/Scripts/ObjectScripts/Elevator.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    private bool _active;
+    private bool _moving; 
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private Transform _target;
+    private Vector3 _position;
+
+    private void Start()
+    {
+        _position = transform.position;
+    }
+    void FixedUpdate()
+    {
+        if(_active == true)
+        {
+            transform.position = Vector3.Lerp(transform.position, _target.position, _speed * Time.deltaTime);
+        }
+        else if(_active == false && _moving == false)
+        {
+            transform.position = Vector3.Lerp(transform.position, _position, _speed * Time.deltaTime);
+        }
+    }
+
+    public void Activate(bool value = true)
+    {
+        if(value == true)
+        {
+            _active = true;
+        }
+        else
+        {
+            _active = false;
+        }
+        
+    }
+
+    public void MoveByRotation(float amount)
+    {
+        float _distance = Vector3.Distance(transform.position, _target.position);
+        Vector3 temp = transform.position;
+        _moving = true;
+        transform.Translate((transform.position - _target.position) * (amount * Time.deltaTime));
+        if(Vector3.Distance(transform.position, _target.position) > _distance)
+        {
+            transform.position = temp;
+        }
+
+    }
+
+}
